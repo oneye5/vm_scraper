419ec9992be3ce72268afbd7cd1707aae95c3935
Owen <ocjlaz@gmail.com>
2024-06-11 13:42:38 +1200

    working mouse control using xlib system calls

--- inputHandler.h
+++ inputHandler.h
@@ -5,12 +5,15 @@
 #include <cstring>
 #include <cerrno>
 #include "CharToKeyCode.h"
+#include <cstdlib>
+
+
 
 class InputHandler
 {
-    bool debug = true;
     int fd;
     uinput_user_dev uidev{};
+
 public:
     void setKey(int keycode, int value) const
     {
@@ -34,29 +37,6 @@ public:
             exit(-1);
         }
     }
-    void sendMouseEvent(int type, int code, int value) const
-    {
-        input_event ev;
-        memset(&ev, 0, sizeof(ev));
-        ev.type = type;
-        ev.code = code;
-        ev.value = value;
-        if (write(fd, &ev, sizeof(ev)) < 0) {
-            std::cerr << "Error writing mouse event: " << strerror(errno) << std::endl;
-            close(fd);
-            exit(-1);
-        }
-
-        // Send the sync event to ensure the change is registered.
-        ev.type = EV_SYN;
-        ev.code = SYN_REPORT;
-        ev.value = 0;
-        if (write(fd, &ev, sizeof(ev)) < 0) {
-            std::cerr << "Error writing sync event: " << strerror(errno) << std::endl;
-            close(fd);
-            exit(-1);
-        }
-    }
     void quickPress(int keyCode) const
     {
         usleep(10000); // 0.01 second
@@ -69,20 +49,22 @@ public:
         for (int i = 0; i < in.length(); i++)
             quickPress( char_to_keycode(in.at(i)) );
     }
-    void setMousePosition(int screenX,int screenY) const
+    void setMousePosition(int x = 0, int y = 0 ) const
     {
-        std::cout << "setting mouse pos\n";
-        sendMouseEvent(EV_ABS, ABS_X, screenX);
-        sendMouseEvent(EV_ABS, ABS_Y, screenY);
+        system(  ("xdotool mousemove " + std::to_string(x) + " " + std::to_string(y)).c_str());
     }
-    void addMousePosition(int deltaX,int deltaY) const
+    void changeMousePosition(int x = 0, int y = 0)
     {
-        std::cout << "adding mouse pos\n";
-        sendMouseEvent(EV_REL, REL_X, deltaX);
-        sendMouseEvent(EV_REL, REL_Y, deltaY);
+        std::cout<<"mouse pos after move: " << system("xdotool getmouselocation") << "\n";
+        system(  ("xdotool mousemove_relative " + std::to_string(x) + " " + std::to_string(y)).c_str());
+        std::cout<<"mouse pos before move: " << system("xdotool getmouselocation") << "\n";
     }
     InputHandler()
     {
+        //get screen dimensions for mouse input
+
+        //setup virtual input device for keyboard events
+
         fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
         if (fd < 0){std::cerr << "Error opening /dev/uinput: " << strerror(errno) << std::endl; exit(-1);} //check for error
 
@@ -93,14 +75,6 @@ public:
         for(auto mapped_key_code : CHAR_KEYCODE_MAP)
             ioctl(fd, UI_SET_KEYBIT, mapped_key_code.second);
 
-        //enable mouse stuff
-        ioctl(fd, UI_SET_EVBIT, EV_REL); // Enable relative events (for mouse movement)
-        ioctl(fd, UI_SET_RELBIT, REL_X); // Enable X-axis movement
-        ioctl(fd, UI_SET_RELBIT, REL_Y); // Enable Y-axis movement
-
-        ioctl(fd, UI_SET_EVBIT, EV_ABS); // Enable non relative
-        ioctl(fd, UI_SET_ABSBIT, ABS_Y); // Enable X-axis movement
-        ioctl(fd, UI_SET_ABSBIT, ABS_X); // Enable Y-axis movement
 
         memset(&uidev, 0, sizeof(uidev));
         snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "uinput-sample");
--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include "inputHandler.h"
 int main()
 {
-    std::cout<<"enter any key to continue";
+    std::cout<<"ensure resolution is set to 1360 * 768\n";
+    system("xdotool mousemove_relative 1 1");
+    std::cout<<"If asked by the OS allow input sharing\n";
+    std::cout<<"enter any key to continue\n";
+
     std::cin.ignore();
 
 
     //start programs
+    std::cout<<"starting in 5 secconds";
+    usleep(500000);
+
     auto* input = new InputHandler();
-    usleep(100000);
+
+    system("sudo firefox");
+    usleep(200000);
+
+
 
     input->quickPress(KEY_A);
     input->quickPress(KEY_B);
@@ -16,13 +27,9 @@ int main()
 
     input->inputString("\nhello, how are you, i am under the water, please help me, here too much raining :(\n");
 
-    input->setMousePosition(0,0);
-    usleep(100000);
-    input->setMousePosition(600,600);
-    usleep(100000);
-    input->addMousePosition(100,100);
-    usleep(100000);
-    input->addMousePosition(100,100);
+
+
+    input->setMousePosition(500,500);
 
 
     delete(input);
